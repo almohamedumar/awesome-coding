5ba4d7f54ec94309c6b68c2020bcc3829459a01d
umar35 <umaralmohamed@gmail.com>
2021-07-13 10:50:10 +0530

    Day 3 longest increasing subsequence prg

--- longest increasing subsequence.cpp
+++ longest increasing subsequence.cpp
@@ -1,41 +1,39 @@
-#include <iostream>
-#include <string.h>
-using namespace std;
-
-
-int LIS( int arr[], int n )  
-{  
-    int lis[n]; 
-   
-    lis[0] = 1;    
-  
-  
-    for (int i = 1; i < n; i++ )  
-    { 
-        lis[i] = 1; 
-        for (int j = 0; j < i; j++ )   
-            if ( arr[i] > arr[j])  
-                lis[i] = max(lis[i], lis[j] + 1);  
-    } 
-    
-    int res = lis[0];
-    
-    for(int i=0;i<n;i++)
+class Solution {
+public:
+    int ceildx(int tail[],int l,int r,int key)
     {
-        res = max(lis[i], res);
+        while(r>l)
+        {
+            int m = l + (r-l) /2;
+            if(tail[m]>=key)
+            {
+                r=m;
+            }
+            else
+            {
+                l = m+1;
+            }
+        }
+        return r;
     }
-    
-    return res;
- 
-}  
-
-int main() {
-	
-	
-int arr[] ={3, 4, 2, 8, 10, 5, 1};
-int n = 7;
-
-cout<<LIS(arr, n);
-	
-
-}
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        int tail[n];
+        int len=1;
+        tail[0] =nums[0];
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]>tail[len-1])
+            {
+                tail[len] = nums[i];
+                len++;
+            }
+            else
+            {
+                int c = ceildx(tail,0,len-1,nums[i]);
+                tail[c] = nums[i];
+            }
+        }
+        return len;
+    }
+};
